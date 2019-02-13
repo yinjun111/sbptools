34cfedade4a0a04bf92475039909109772dff92a
Jun Yin <jyin@sbpdiscovery.org>
2019-02-13 15:20:13 -0800

    running version for cutadapt to rsem

--- rnaseq-process/rnaseq-process_caller.pl
+++ rnaseq-process/rnaseq-process_caller.pl
@@ -2,6 +2,7 @@
 use strict;
 use Getopt::Long;
 use Cwd qw(abs_path);
+use File::Basename qw(basename);
 
 #CutAdapt+FASTQC+RSEM+STAR
 
@@ -33,10 +34,16 @@ Usage: sbptools rnaseq-process [parameters]
 
 Parameters:
 
-    -c/--config       Configuration file
-    -o/--output       Output folder
+    --config|-c       Configuration file
+    --output|-o       Output folder
 
+    --tx|-t           Transcriptome
+                        Current support Human.B38.Ensembl84, Mouse.B38.Ensembl84
 
+    --runmode|-r      Where to run the scripts, local, server or none [none]
+    --verbose|-v      Verbose
+	
+	
 ";
 
 
@@ -58,10 +65,14 @@ my $params=join(" ",@ARGV);
 my $configfile;
 my $outputfolder;
 my $verbose;
+my $tx;
+my $runmode="none";
 
 GetOptions(
 	"config|c=s" => \$configfile,
 	"output|o=s" => \$outputfolder,
+	"tx|t=s" => \$tx,	
+	"runmode|r=s" => \$runmode,		
 	"verbose|v" => \$verbose,
 );
 
@@ -73,9 +84,16 @@ if(!-e $outputfolder) {
 
 
 my $scriptfolder="$outputfolder/scripts";
+
+if(!-e $scriptfolder) {
+	mkdir($scriptfolder);
+}
+
+
 my $logfile="$outputfolder/rnaseq-process_run.log";
 my $newconfigfile="$outputfolder/rnaseq-process_config.txt";
 
+my $scriptfile1="$scriptfolder/rnaseq-process_run.sh";
 
 #write log file
 open(LOG, ">$logfile") || die "Error writing into $logfile. $!";
@@ -89,7 +107,25 @@ print LOG "$cutadapt version:", getsysoutput("$cutadapt --version"),"\n";
 print LOG "$fastqc version:", getsysoutput("$fastqc --version"),"\n";
 print LOG "$rsem version:", getsysoutput("$rsem --version"),"\n";
 print LOG "$star version:", getsysoutput("$star --version"),"\n";
+print LOG "\n";
 
+
+#test tx option
+
+my %tx2ref=(
+	"Human.B38.Ensembl84"=>"/home/jyin/Projects/Databases/Ensembl/v84/Human_STAR/Human_RSEM",
+	"Mouse.B38.Ensembl84"=>"/home/jyin/Projects/Databases/Ensembl/v84/Mouse_STAR/Mouse_RSEM",
+);
+
+
+if(defined $tx2ref{$tx}) {
+	print STDERR "Starting analysis using $tx.\n\n" if $verbose;
+	print LOG "Starting analysis using $tx.\n\n";
+}
+else {
+	print STDERR "ERROR:$tx not defined. Currently only supports ",join(",",sort keys %tx2ref),"\n\n" if $verbose;
+	print LOG "ERROR:$tx not defined. Currently only supports ",join(",",sort keys %tx2ref),"\n\n";
+}
 
 
 ########
@@ -185,17 +221,147 @@ while(<IN>) {
 close IN;
 close OUT;
 
-			
 
+#----------------
+#create folder
+print STDERR scalar(keys %sample2fastq)," samples identified from $configfile, including:\nSAMPLE\tINDEX\n",join("\n",map {$_."\t".$sample2indexname{$_}} sort keys %sample2indexname),"\n\n" if $verbose;
+print LOG scalar(keys %sample2fastq)," samples identified from $configfile, including:\nSAMPLE\tINDEX\n",join("\n",map {$_."\t".$sample2indexname{$_}} sort keys %sample2indexname),"\n\n" if $verbose;
 
+print STDERR "Make folders for different samples.\n\n" if $verbose;
+print LOG "Make folders for different samples.\n\n";
+
+foreach my $sample (sort keys %sample2fastq) {
+	my $samplefolder="$outputfolder/$sample";
+	if(!-e	$samplefolder) {
+		mkdir $samplefolder;
+	}
+}
+
+#----------------
 #prepare fastq file for cutadapt
 #assume truseq for now
 
 
 
 
+my %sample2workflow;
+
+#FASTQ files should all be .fastq.gz
+
+if(defined $configattrs{"FASTQ2"}) {
+	#PE
+	print STDERR "Printing cutadapt PE script.\n\n" if $verbose;
+	print LOG "Printing cutadapt PE script.\n\n";
+	
+	foreach my $sample (sort keys %sample2fastq) {
+		my $samplefolder="$outputfolder/$sample";
+		
+		my $fastq1=$sample2fastq{$sample}[0];
+		my $fastq2=$sample2fastq{$sample}[0];
+		
+		my $fastq1trim="$samplefolder/".basename($fastq1);
+		$fastq1trim=~s/\.fastq\.gz/trimmed.fastq.gz/;
+		push @{$sample2fastq{$sample}},$fastq1trim;
+
+		my $fastq2trim="$samplefolder/".basename($fastq2);
+		$fastq2trim=~s/\.fastq\.gz/trimmed.fastq.gz/;
+		push @{$sample2fastq{$sample}},$fastq2trim;
+
+		$sample2workflow{$sample}.="$cutadapt -j 4 -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -o $fastq1trim -p $fastq2trim $fastq1 $fastq2;";
+	}
+}
+else {
+	#SE
+	print STDERR "Printing cutadapt SE script.\n\n" if $verbose;
+	print LOG "Printing cutadapt SE script.\n\n";
+	
+	foreach my $sample (sort keys %sample2fastq) {
+		my $samplefolder="$outputfolder/$sample";
+	
+		my $fastq1=$sample2fastq{$sample}[0];
+		
+		my $fastq1trim="$samplefolder/".basename($fastq1);
+		$fastq1trim=~s/\.fastq\.gz/trimmed.fastq.gz/;
+		push @{$sample2fastq{$sample}},$fastq1trim;
+
+		$sample2workflow{$sample}.="$cutadapt -j 4 -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -o $fastq1trim $fastq1;";
+	}
+}
+		
+
+#----------------
+#FASTQC for trimmed reads
 
 
+if(defined $configattrs{"FASTQ2"}) {
+	#PE
+	print STDERR "Printing FASTQC PE script.\n\n" if $verbose;
+	print LOG "Printing FASTQC PE script.\n\n";
+	
+	foreach my $sample (sort keys %sample2fastq) {
+		my $samplefolder="$outputfolder/$sample";
+		
+		$sample2workflow{$sample}.="$fastqc --nogroup -o $samplefolder -f fastq ".$sample2fastq{$sample}[2]." ".$sample2fastq{$sample}[3].";";
+	}
+}
+else {
+	#SE
+	print STDERR "Printing FASTQC SE script.\n\n" if $verbose;
+	print LOG "Printing FASTQC SE script.\n\n";
+	
+	foreach my $sample (sort keys %sample2fastq) {
+		my $samplefolder="$outputfolder/$sample";
+		
+		$sample2workflow{$sample}.="$fastqc --nogroup -o $samplefolder -f fastq ".$sample2fastq{$sample}[1].";";
+	}
+}	
+		
+	
+#----------------
+#RSEM for trimmed reads
+
+if(defined $configattrs{"FASTQ2"}) {
+	#PE
+	print STDERR "Printing RSEM PE script.\n\n" if $verbose;
+	print LOG "Printing RSEM PE script.\n\n";
+	
+	foreach my $sample (sort keys %sample2fastq) {
+		my $samplefolder="$outputfolder/$sample";
+		
+		$sample2workflow{$sample}.="$rsem -p 4 --output-genome-bam --sort-bam-by-coordinate --star-gzipped-read-file --star --paired-end ".$sample2fastq{$sample}[2]." ".$sample2fastq{$sample}[3]." ".$tx2ref{$tx}." $samplefolder/$sample;";
+	}
+}
+else {
+	#SE
+	print STDERR "Printing RSEM SE script.\n\n" if $verbose;
+	print LOG "Printing RSEM SE script.\n\n";
+	
+	foreach my $sample (sort keys %sample2fastq) {
+		my $samplefolder="$outputfolder/$sample";
+		
+		$sample2workflow{$sample}.="$rsem -p 4 --output-genome-bam --sort-bam-by-coordinate --star-gzipped-read-file --star ".$sample2fastq{$sample}[1]." ".$tx2ref{$tx}." $samplefolder/$sample;";
+	}
+}
+
+
+
+########
+#Print out commands, for local and server run
+########
+
+
+open(S1,">$scriptfile1") || die "Error writing $scriptfile1. $!";
+
+foreach my $sample (sort keys %sample2workflow) {
+	print S1 $sample2workflow{$sample},"\n";
+}
+
+close S1;
+
+
+#local mode
+print STDERR "To run locally, in shell type: sh $scriptfile1\n\n";
+print LOG "To run locally, in shell type: sh $scriptfile1\n\n";
 
 
 
