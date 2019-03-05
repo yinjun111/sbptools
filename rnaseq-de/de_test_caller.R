959d4d09c9397e12bd0909b815afc9a6be050f4b
Jun Yin <jyin@sbpdiscovery.org>
2019-03-04 16:41:49 -0800

    de_test now running

--- rnaseq-de/de_test_caller.R
+++ rnaseq-de/de_test_caller.R
@@ -1,6 +1,40 @@
 
-library(DESeq2)
 
+library("argparser",quietly =T)
+
+version="0.1"
+
+description=paste0("de_test\nversion ",version,"\n","Usage:\nDescription: Differential Expression calculation using DESeq2\n")
+
+
+#####
+#Add arguments
+#####
+
+parser <- arg_parser(description=description)
+
+parser <- add_argument(parser, arg="--in",short="-i", type="character", help = "Expr input file")
+parser <- add_argument(parser, arg="--anno",short="-a", type="character", help = "Annotation file")
+parser <- add_argument(parser, arg="--out",short="-o", type="character", help = "Output file")
+parser <- add_argument(parser, arg="--formula",short="-f", type="character", help = "DESeq formula")
+parser <- add_argument(parser, arg="--treat",short="-t", type="character", help = "treatment name")
+parser <- add_argument(parser, arg="--ref",short="-r", type="character", help = "reference name")
+parser <- add_argument(parser, arg="--fccutoff", type="float", help = "Log2 FC cutoff",default=1)
+parser <- add_argument(parser, arg="--qcutoff", type="float", help = "qcutoff",default=0.05)
+parser <- add_argument(parser, arg="--pmethod",type="character", help = "Method used in DESeq2",default="Wald")
+parser <- add_argument(parser, arg="--qmethod",type="character", help = "FDR Method",default="BH")
+
+args = parse_args(parser)
+
+print(args)
+
+#other parameters
+
+core=1
+
+#args_file = "tempArgObjectFile.rds"
+#saveRDS(args, args_file); print(args); quit(); #comment this after creating args_file
+#args = readRDS(args_file)  
 
 
 #####
@@ -26,8 +60,8 @@ filter_data <- function(mat,type="sum",cutoff=10,na.rm=0) {
 #DESeq2
 ######
 
-deseq2_test <- function(mat,anno,design,fc_cutoff=1,q_cutoff=0.05,pmethod="Wald",qmethod="BH",core=4){
-  
+deseq2_test <- function(mat,anno,design,fc_cutoff=1,q_cutoff=0.05,pmethod="Wald",qmethod="BH",core=core,treat,ref){
+  library(DESeq2,quietly =T)
   #anno and design may need to be checked
 
 
@@ -36,7 +70,11 @@ deseq2_test <- function(mat,anno,design,fc_cutoff=1,q_cutoff=0.05,pmethod="Wald"
 	                              design= as.formula(design))
 	dds <- DESeq(dds,test=pmethod)
 	resultsNames(dds) # lists the coefficients
-	res <- results(dds, pAdjustMethod =qmethod)
+	
+	#need to modify here
+	comp<-substring(design,2)
+	
+	res <- results(dds, pAdjustMethod =qmethod,contrast=c(comp,treat,ref))
 	
 	fc<-res[,2]
 	p<-res[,5]
@@ -71,12 +109,13 @@ deseq2_test <- function(mat,anno,design,fc_cutoff=1,q_cutoff=0.05,pmethod="Wald"
 #####
 
 
-setwd("D:\\Work\\SBP\\sbptools-dev")
-
-data<-read.table("gene.results.merged.count.txt",header=T,row.names=1,sep="\t")
-anno<-read.table("Sample_Anno.txt",header=T,row.names=1,sep="\t")
+data<-read.table(args$"in",header=T,row.names=1,sep="\t")
+anno<-read.table(args$anno,header=T,row.names=1,sep="\t")
 
 
 data.sel<-filter_data(data)
 
-data.sel.result<-deseq2_test(data.sel,anno=anno,design="~Tissue")
+data.sel.result<-deseq2_test(mat=data.sel,anno=anno,design=args$formula,fc_cutoff=args$fccutoff,q_cutoff=args$q_cutoff,pmethod=args$pmethod,qmethod=args$qmethod,treat=args$treat,ref=args$ref)
+
+write.table(data.sel.result,file=args$out,sep="\t",quote=F,col.names = NA)
+
