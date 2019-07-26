380c6b70a64cc9ad29276b071853931aa8c1fc9d
Jun Yin <jyin@sbpdiscovery.org>
2019-07-25 17:48:31 -0700

    v0.3 update for sbptools caller

--- sbptools_caller.pl
+++ sbptools_caller.pl
@@ -4,9 +4,9 @@ use Getopt::Long;
 
 
 
-my $version="0.2";
+my $version="0.3";
 
-
+#v0.3, runmode implementations in rnaseq and chipseq
 
 my $usage="
 
@@ -22,11 +22,15 @@ Parameters:
     rnaseq-merge      Merge rnaseq-process results for downstream analyses
     rnaseq-de         Perform DE analysis using DESeq2
     rnaseq-summary    Summarize RNA-Seq DE results
+
+    rnaseq-var        RNA-seq variant calling pipeline
     
     chipseq-process   ChIP-seq QC, Align, and Peak Calling
     chipseq-merge     Summarize ChIP-Seq results
     chipseq-de        Perform DE analysis for ChIP-Seq
     chipseq-summary   Summarize ChIP-seq DE results
+
+    motif-finder      Transcription factor binding motif prediction
 	
     bs-fastq          Download and merge FASTQ files from Basespace	
 
@@ -54,10 +58,14 @@ my $rnaseq_merge="/apps/sbptools/rnaseq-merge/rnaseq-merge_caller.pl";
 my $rnaseq_de="/apps/sbptools/rnaseq-de/rnaseq-de_caller.pl";
 my $rnaseq_summary="/apps/sbptools/rnaseq-summary/rnaseq-summary_caller.pl";
 
+my $rnaseq_var="sh /apps/sbptools/rnaseq-var/gatk3_rnaseq_variant_v1.sh";
+
 my $chipseq_process="/apps/sbptools/chipseq-process/chipseq-process_caller.pl";
 my $chipseq_merge="/apps/sbptools/chipseq-merge/chipseq-merge_caller.pl";
 my $chipseq_de="/apps/sbptools/chipseq-de/chipseq-de_caller.pl";
 my $chipseq_summary="/apps/sbptools/chipseq-summary/chipseq-summary_caller.pl";
+
+my $motif_finder="/apps/sbptools/motif-finder/motif-finder_caller.pl";
 
 my $mergefiles="/apps/sbptools/mergefiles/mergefiles_caller.pl";
 my $text2excel ="perl /apps/sbptools/text2excel/text2excel.pl";
@@ -70,11 +78,15 @@ my %commands2program=(
     "rnaseq-merge"=>$rnaseq_merge,
     "rnaseq-de"=>$rnaseq_de,
 	"rnaseq-summary"=>$rnaseq_summary,
+
+	"rnaseq-var"=>$rnaseq_var,
 	
     "chipseq-process"=>$chipseq_process,
     "chipseq-merge"=>$chipseq_merge,
     "chipseq-de"=>$chipseq_de,
 	"chipseq-summary"=>$chipseq_summary,	
+
+	"motif-finder"=>$motif_finder,
     
 	"mergefiles"=>$mergefiles,
     "text2excel"=>$text2excel,
