9117f829cb2091fcc198cc13abc69ba7201f1028
Jun Yin <jyin@sbpdiscovery.org>
2020-08-26 15:06:46 -0700

    rnaseq-de v0.5 add volcano plot

--- rnaseq-de/de_test_caller.R
+++ rnaseq-de/de_test_caller.R
@@ -2,11 +2,13 @@
 
 library("argparser",quietly =T)
 
-version="0.3"
+version="0.4"
 
 #0.2b, change auto filter to *5. Add indfilter and cookscutoff option
 #0.23, add write_table_proper
 #0.3, add MA plot,fixed xlim for volcano plot
+#0.31, add plotting option
+#0.4 add new volcano plot and gene annotation. --anno and --config are used
 
 description=paste0("de_test\nversion ",version,"\n","Usage:\nDescription: Differential Expression calculation using DESeq2\n")
 
@@ -18,7 +20,8 @@ description=paste0("de_test\nversion ",version,"\n","Usage:\nDescription: Differ
 parser <- arg_parser(description=description)
 
 parser <- add_argument(parser, arg="--in",short="-i", type="character", help = "Expr input file")
-parser <- add_argument(parser, arg="--anno",short="-a", type="character", help = "Annotation file")
+parser <- add_argument(parser, arg="--config",short="-c", type="character", help = "Config file for samples")
+parser <- add_argument(parser, arg="--anno",short="-a", type="character", help = "Annotation file for genes")
 parser <- add_argument(parser, arg="--out",short="-o", type="character", help = "Output file")
 parser <- add_argument(parser, arg="--formula",short="-f", type="character", help = "DESeq formula")
 parser <- add_argument(parser, arg="--treat",short="-t", type="character", help = "treatment name")
@@ -30,7 +33,7 @@ parser <- add_argument(parser, arg="--qmethod",type="character", help = "FDR Met
 parser <- add_argument(parser, arg="--filter",type="float", help = "Count filter for all the exps",default="10")
 parser <- add_argument(parser, arg="--independentfiltering",type="logical", help = "DESeq2 independentFiltering",default=T)
 parser <- add_argument(parser, arg="--cookscutoff",type="logical", help = "DESeq2 cooksCutoff",default=T)
-
+parser <- add_argument(parser, arg="--plot",type="logical", help = "Whether to generate volcano and MA plots",default=T)
 
 args = parse_args(parser)
 
@@ -69,14 +72,16 @@ filter_data <- function(mat,type="sum",cutoff=10,na.rm=0) {
 		}
 	}
 
-		
+	#changed here to return T/F	
 	if(type == "sum") {
-		mat.sel<-mat[apply(mat,1,sum)>=num.cutoff,]
+		#mat.sel<-mat[apply(mat,1,sum)>=num.cutoff,]
+		return(apply(mat,1,sum)>=num.cutoff)
 	} else if (type=="none") {
-		mat.sel<-mat
+		#mat.sel<-mat
+		return(rep(TRUE,nrow(mat)))
 	}
 	
-	return(mat.sel)
+	#return(mat.sel)
 }
 
 
@@ -193,6 +198,91 @@ volcano_plot_ggplot<-function(fc,q,sig,xlim=c(-7,7),ylim=c(0,30),xlab="Log2FC",y
   
 }
 
+enhanced_volcano_plot <- function(gene, fc, q, sig, labels = NULL, 
+                                  upcol = "red2", downcol = "blue2",
+                                  xlab = "Log2FC", ylab = "-log10 P", main = "Volcano Plot", 
+                                  fc_cutoff = args$fccutoff, q_cutoff = args$q_cutof){
+    
+  library(EnhancedVolcano)
+  library(ggplot2)
+  
+  # Create a data frame using gene names, fold changes, and q-values
+  fc <- as.numeric(unlist(fc))
+  q <- as.numeric(unlist(q))
+  q[is.na(q)] <- 1
+  
+  gene <- make.names(as.character(unlist(gene)),unique = T) #changed here to allow duplicated gene names
+
+  df <- data.frame(gene_name = as.character(gene), lfc = fc, q = q, stringsAsFactors = F)
+  rownames(df) <- gene
+   
+  
+  # Determine balanced x and y-axis limits (no features omitted)
+  # y-axis upper limit
+  if(min(q)>0) {
+    max_pval <- -log10(min(q)) + 0.1
+  } else {
+    max_pval <- -log10(min(q[q>0])/10) + 0.1 #changed here for pval=0
+  }
+  
+  
+  # To get a symmetrical x-axis
+  max_fc <- max(fc, na.rm = T)
+  min_fc <- min(fc, na.rm = T)
+  if (max_fc > abs(min_fc)){
+    min_fc <- -max_fc
+  }else{
+    max_fc <- -min_fc
+  }
+  
+  # Create a named vector of custom colors
+  col_scheme <- c("Up"=upcol, "Down"=downcol, "N.S."="grey")
+  cols <- rep(col_scheme['N.S.'], length(gene))
+  cols[sig == 1] <- col_scheme['Up']
+  cols[sig == -1] <- col_scheme['Down']
+  names(cols)[cols == col_scheme['Up']] <- "Up"
+  names(cols)[cols == col_scheme['Down']] <- "Down"
+  names(cols)[cols == col_scheme['N.S.']] <- "N.S."
+  
+  # Label top 10 Up and down genes (based on FC) if user does not 
+  # provide a vector of genes to label on volcano plot
+  if (is.null(labels) == TRUE){
+    tmp.df <- data.frame(gene, fc, cols)
+    rownames(tmp.df) <- gene
+    tmp.df <- tmp.df[tmp.df$cols != col_scheme['N.S.'],]
+    tmp.df <- tmp.df[order(tmp.df$fc),]
+    labels <- c(rownames(tmp.df)[1:10], tail(rownames(tmp.df), n = 10))
+  }
+
+  # Render the volcano plot
+  plt <- EnhancedVolcano(df, x = 'lfc', y = 'q', lab = df$gene_name,
+                         pCutoff = q_cutoff, FCcutoff = fc_cutoff, 
+                         gridlines.major = FALSE, gridlines.minor = FALSE, 
+                         drawConnectors = T, legendLabSize = 12,
+                         cutoffLineCol = "red", colAlpha = 0.75, 
+                         cutoffLineType = "dashed", border = "full",
+                         colCustom = cols, legendPosition = "right",
+                         pointSize = 2, cutoffLineWidth = 0.4,
+                         labFace = "plain", subtitle = main,
+                         ylim = c(0, max_pval), xlim = c(min_fc, max_fc),
+                         axisLabSize = 12, captionLabSize = 12, 
+                         xlab = xlab, ylab = ylab, title = "", 
+                         caption = paste0('Total = ', nrow(df), ' genes'),
+                         typeConnectors = "closed", legendIconSize = 2,
+                         selectLab = labels, borderWidth = 1.5
+                         )
+  
+  # Add numbers of up and down DE genes to the plot
+  down <- as.character(table(cols)[col_scheme['Down']])
+  up <- as.character(table(cols)[col_scheme['Up']])
+  
+  plt <- plt + geom_text(x=min_fc, y=max_pval, label=down, 
+                         col = col_scheme['Down'], size = 5)
+  plt <- plt + geom_text(x=max_fc, y=max_pval, label=up, 
+                         col = col_scheme['Up'], size = 5)
+  
+  print(plt) #generate plot
+}
 
 #to be implemented
 ma_plot_ggplot<-function(m,a,sig,xlim=c(1,30),ylim=c(-7,7),xlab="A:Log2 Mean of Normalized Counts",ylab="M:Log2 Fold Change",main="MA Plot",m_cutoff=args$fc_cutof) {
@@ -284,18 +374,29 @@ write_table_proper<-function(file,data,name="Gene") {
 #Statistical test
 #####
 
-
+#anno and config are changed in v0.6
 data<-read.table(args$"in",header=T,row.names=1,sep="\t",check.names=F,flush=T)
-anno<-read.table(args$anno,header=T,row.names=1,sep="\t",check.names=F,flush=T)
+config<-read.table(args$config,header=T,row.names=1,sep="\t",check.names=F,flush=T)
+
+if(!is.na(args$anno)) {
+	anno<-read.table(args$anno,header=T,row.names=1,sep="\t",check.names=F,comment.char="",quote="",flush=T)
+	anno<-as.vector(unlist(anno[,1]))
+} else {
+	anno<-rownames(data)
+}
 
 #can be customized later
-data.sel<-filter_data(data,cutoff=args$filter)
+data.sel.rows<-filter_data(data,cutoff=args$filter)
+data.sel<-data[data.sel.rows,]
+
+anno.sel<-anno[data.sel.rows]
+
 
 #save image
 rdatafile=sub(".txt$",".rdata",args$out,perl=T)
 
 
-data.sel.result<-deseq2_test(mat=data.sel,anno=anno,design=args$formula,fc_cutoff=args$fccutoff,q_cutoff=args$qcutoff,pmethod=args$pmethod,qmethod=args$qmethod,treat=args$treat,ref=args$ref,independentfiltering=args$independentfiltering,cookscutoff=args$cookscutoff)
+data.sel.result<-deseq2_test(mat=data.sel,anno=config,design=args$formula,fc_cutoff=args$fccutoff,q_cutoff=args$qcutoff,pmethod=args$pmethod,qmethod=args$qmethod,treat=args$treat,ref=args$ref,independentfiltering=args$independentfiltering,cookscutoff=args$cookscutoff)
 
 #write.table(data.sel.result$result,file=args$out,sep="\t",quote=F,col.names = NA)
 
@@ -308,37 +409,59 @@ save.image(file=rdatafile)
 #Plots
 #####
 
-#volcano plot
-vp_outfile_pdf=sub("\\.\\w+$","_volcanoplot.pdf",args$out,perl=T)
+if(args$plot) {
 
-pdf(vp_outfile_pdf,width=7, height=6)
-volcano_plot_ggplot(fc=data.sel.result$result[,1],q=data.sel.result$result[,4],sig=data.sel.result$result[,5],xlab=colnames(data.sel.result$result)[1],ylab=paste("-Log10 ",colnames(data.sel.result$result)[4],sep=""),main=paste("Volcano Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),q_cutoff=args$qcutoff,fc_cutoff = args$fccutoff)
-dev.off()
+	library(Cairo)
+	
+	#volcano plot
+	vp_outfile_pdf=sub("\\.\\w+$","_volcanoplot.pdf",args$out,perl=T)
 
-#turned off for now, no X11 in linux
-
-#vp_outfile_jpg=sub("\\.\\w+$","_volcanoplot.jpg",args$out,perl=T)
-
-#jpeg(vp_outfile_jpg,width=7, height=6, units="in", res=300) 
-#volcano_plot_ggplot(fc=data.sel.result$result[,1],q=data.sel.result$result[,4],sig=data.sel.result$result[,5],xlab=colnames(data.sel.result$result)[1],ylab=paste("-Log10 ",colnames(data.sel.result$result)[4],sep=""),main=paste("Volcano Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),q_cutoff=args$qcutoff,fc_cutoff = args$fccutoff)
-#dev.off()
+	#older version
+	#pdf(vp_outfile_pdf,width=7, height=6)
+	#volcano_plot_ggplot(fc=data.sel.result$result[,1],q=data.sel.result$result[,4],sig=data.sel.result$result[,5],xlab=colnames(data.sel.result$result)[1],ylab=paste("-Log10 ",colnames(data.sel.result$result)[4],sep=""),main=paste("Volcano Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),q_cutoff=args$qcutoff,fc_cutoff = args$fccutoff)
+	#dev.off()
 
 
-#####
-#Plots
-#####
-
-#volcano plot
-mp_outfile_pdf=sub("\\.\\w+$","_maplot.pdf",args$out,perl=T)
-
-pdf(mp_outfile_pdf,width=7, height=6)
-ma_plot_ggplot(m=data.sel.result$result[,1],a=log2(data.sel.result$res[,1]),sig=data.sel.result$result[,5],ylab=paste("M:",colnames(data.sel.result$result)[1],sep=""),main=paste("MA Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),m_cutoff = args$fccutoff)
-dev.off()
+	#Volcano plot, pdf
+	vp_outfile2_pdf=sub("\\.\\w+$","_volcanoplot2.pdf",args$out,perl=T)
+	
+	
+	
+	pdf(vp_outfile2_pdf,width=7, height=6)
+	enhanced_volcano_plot(gene=anno.sel,fc=data.sel.result$result[,1],q=data.sel.result$result[,4],sig=data.sel.result$result[,5],xlab=colnames(data.sel.result$result)[1],ylab=paste("-Log10 ",colnames(data.sel.result$result)[4],sep=""),main=paste("Volcano Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),q_cutoff=args$qcutoff,fc_cutoff = args$fccutoff,upcol="red2",downcol="green2")
+	dev.off()
 
 
-#mp_outfile_jpg=sub("\\.\\w+$","_maplot.jpg",args$out,perl=T)
+	#Volcano plot, png
+	vp_outfile_png=sub("\\.\\w+$","_volcanoplot.png",args$out,perl=T)
 
-#jpeg(mp_outfile_jpg,width=7, height=6, units="in", res=300)
-#ma_plot_ggplot(m=data.sel.result$result[,1],a=log2(data.sel.result$res[,1]),sig=data.sel.result$result[,5],ylab=paste("M:",colnames(data.sel.result$result)[1],sep=""),main=paste("MA Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),m_cutoff = args$fccutoff)
-#dev.off()
+	CairoPNG(filename = vp_outfile_png,res = 300,width=2500, height=2200)
 
+	enhanced_volcano_plot(gene=anno.sel,fc=data.sel.result$result[,1],q=data.sel.result$result[,4],sig=data.sel.result$result[,5],xlab=colnames(data.sel.result$result)[1],ylab=paste("-Log10 ",colnames(data.sel.result$result)[4],sep=""),main=paste("Volcano Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),q_cutoff=args$qcutoff,fc_cutoff = args$fccutoff)
+
+	dev.off()
+
+
+	#####
+	#Plots
+	#####
+
+	#MA plot, pdf
+	mp_outfile_pdf=sub("\\.\\w+$","_maplot.pdf",args$out,perl=T)
+
+	pdf(mp_outfile_pdf,width=7, height=6)
+	ma_plot_ggplot(m=data.sel.result$result[,1],a=log2(data.sel.result$res[,1]),sig=data.sel.result$result[,5],ylab=paste("M:",colnames(data.sel.result$result)[1],sep=""),main=paste("MA Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),m_cutoff = args$fccutoff)
+	dev.off()
+
+	#MA plot, jpg
+	#mp_outfile_jpg=sub("\\.\\w+$","_maplot.jpg",args$out,perl=T)
+
+	#CairoJPEG(filename = mp_outfile_jpg,res = 300)
+
+	#ma_plot_ggplot(m=data.sel.result$result[,1],a=log2(data.sel.result$res[,1]),sig=data.sel.result$result[,5],ylab=paste("M:",colnames(data.sel.result$result)[1],sep=""),main=paste("MA Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),m_cutoff = args$fccutoff)
+	#dev.off()
+	
+	#jpeg(mp_outfile_jpg,width=7, height=6, units="in", res=300)
+	#ma_plot_ggplot(m=data.sel.result$result[,1],a=log2(data.sel.result$res[,1]),sig=data.sel.result$result[,5],ylab=paste("M:",colnames(data.sel.result$result)[1],sep=""),main=paste("MA Plot ","Significance: Log2FC ",round(args$fccutoff,2)," ",args$qmethod, "P ",args$qcutoff,sep=""),m_cutoff = args$fccutoff)
+	#dev.off()
+}
